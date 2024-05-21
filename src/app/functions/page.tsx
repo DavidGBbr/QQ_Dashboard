b152483974514c34c4a8cfa9606f566d6fa68a72
davidGBbr <davidgomesbrigido@gmail.com>
2024-05-21 14:07:55 -0300

    function page

---
+++ src/app/functions/page.tsx
@@ -0,0 +1,45 @@
+import ListItems from "@/components/ListItems";
+import Sidenav from "@/components/Sidenav";
+import React from "react";
+import "../../styles/form.css";
+import { MdReceiptLong } from "react-icons/md";
+
+const Functions = () => {
+  const functions = [
+    {
+      id: 0,
+      name: "Visualizar Dados Pessoais - (Gerenciar Dados de Funcionários)",
+    },
+    {
+      id: 1,
+      name: "Aprovar Alterações Salariais - (Gerenciar Salários)",
+    },
+    {
+      id: 2,
+      name: "Adicionar Produto - (Gerenciar estoque)",
+    },
+    {
+      id: 3,
+      name: "Registrar Recebimento - (Registrar Entrada de Produtos)",
+    },
+  ];
+  return (
+    <Sidenav>
+      <main className="container">
+        <div className="page-header">
+          <h2>Funções</h2>
+          <button className="button-green">Registrar</button>
+        </div>
+        <div className="search-input">
+          <input type="text" placeholder="Digite o nome da função..." />
+          <button className="button-green">Filtrar</button>
+        </div>
+        <div>
+          <ListItems items={functions} ItemIcon={MdReceiptLong} />
+        </div>
+      </main>
+    </Sidenav>
+  );
+};
+
+export default Functions;
